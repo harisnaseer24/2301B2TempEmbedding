6183f21dce567451acc5b709fdd990d02d10b759
harisnaseer24 <harisnaseer258@gmail.com>
2024-06-24 13:30:33 +0500

    Client and Admin template added + Model with data annotations

---
+++ 2301B2TempEmbedding/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using _2301B2TempEmbedding.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2301B2TempEmbedding.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        public IActionResult Addprod()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Addprod(Product prd)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("data is all good");
+            }
+            return View();
+        }
+    }
+}
---
+++ 2301B2TempEmbedding/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2301B2TempEmbedding.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+   
+      
+    }
+}
