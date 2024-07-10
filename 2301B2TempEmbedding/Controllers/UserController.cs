fedfd82b9009382d73e4ca726491614feec49dba
harisnaseer24 <harisnaseer258@gmail.com>
2024-07-10 11:25:19 +0500

    edit and delete operations completed

--- 2301B2TempEmbedding/Controllers/UserController.cs
+++ 2301B2TempEmbedding/Controllers/UserController.cs
@@ -32,5 +32,42 @@ namespace _2301B2TempEmbedding.Controllers
             return View();
             }
         }
+        public IActionResult Edit(int id)
+        {
+            var User = db.Users.Find(id);
+            return View(User);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(User abc)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Update(abc);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Delete(int id)
+        {
+            var User = db.Users.Find(id);
+            return View(User);
+        }
+        [HttpPost]
+        public IActionResult Delete(User del)
+        {
+            
+                db.Users.Remove(del);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+        }
+
     }
-}
+
