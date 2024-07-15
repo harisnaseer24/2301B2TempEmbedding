634e269f8fdc9623e54a908142ea80c991c99a46
harisnaseer24 <harisnaseer258@gmail.com>
2024-07-15 11:24:52 +0500

    create operation with file uploading

--- 2301B2TempEmbedding/Controllers/ItemController.cs
+++ 2301B2TempEmbedding/Controllers/ItemController.cs
@@ -1,6 +1,7 @@
 ﻿using _2301B2TempEmbedding.Models;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace _2301B2TempEmbedding.Controllers
@@ -18,5 +19,40 @@ namespace _2301B2TempEmbedding.Controllers
             var ItemsData = _db.Items.Include(p =>  p.Cat);
             return View(ItemsData);
         }
+
+        public IActionResult Create()
+        {
+            ViewBag.CatId = new SelectList(_db.Categories, "CatId", "CatName");
+
+           
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item, IFormFile file)
+        {
+            var imageName = DateTime.Now.ToString("yymmddhhmmss");//24074455454454
+            imageName += Path.GetFileName(file.FileName);//24074455454454apple.png
+
+            string imagepath = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/Uploads");
+            var imagevalue = Path.Combine(imagepath, imageName);
+
+            using (var stream = new FileStream(imagevalue, FileMode.Create)) {
+
+                file.CopyTo(stream);
+
+            }
+
+            var dbimage = Path.Combine("/Uploads",imageName);//   /uploads/240715343434apple.png
+            item.Pimage = dbimage;
+
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            
+            ViewBag.CatId = new SelectList(_db.Categories, "CatId","CatName");
+            return RedirectToAction("Index");
+        }
+
     }
 }
