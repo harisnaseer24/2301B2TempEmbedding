13d1080d84edb6d85099a593394fc82fb78ad141
harisnaseer24 <harisnaseer258@gmail.com>
2024-07-19 11:19:38 +0500

    edit operation with file uploading

--- 2301B2TempEmbedding/Controllers/ItemController.cs
+++ 2301B2TempEmbedding/Controllers/ItemController.cs
@@ -46,13 +46,71 @@ namespace _2301B2TempEmbedding.Controllers
 
             var dbimage = Path.Combine("/Uploads",imageName);//   /uploads/240715343434apple.png
             item.Pimage = dbimage;
-
-            _db.Items.Add(item);
-            _db.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                 _db.Items.Add(item);
+                 _db.SaveChanges();
+            }
+            else
+            {
+                return View();
+            }
+         
             
             ViewBag.CatId = new SelectList(_db.Categories, "CatId","CatName");
             return RedirectToAction("Index");
         }
+
+        //Edit
+        public IActionResult Edit(int id)
+        {
+            ViewBag.CatId = new SelectList(_db.Categories, "CatId", "CatName");
+            var item1 = _db.Items.Find(id);
+
+            return View(item1);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Item item, IFormFile file, string oldimage)
+        {
+            var dbimage = "";
+
+            if(file != null && file.Length > 0)
+            {
+                var imageName = DateTime.Now.ToString("yymmddhhmmss");//24074455454454
+                imageName += Path.GetFileName(file.FileName);//24074455454454apple.png
+
+                string imagepath = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/Uploads");
+                var imagevalue = Path.Combine(imagepath, imageName);
+
+                using (var stream = new FileStream(imagevalue, FileMode.Create))
+                {
+
+                    file.CopyTo(stream);
+
+                }
+
+                dbimage = Path.Combine("/Uploads", imageName);//   /uploads/240715343434apple.png
+                item.Pimage = dbimage;
+                _db.Items.Update(item);
+                _db.SaveChanges();
+
+
+
+            }
+            else
+            {
+                item.Pimage = oldimage;
+                _db.Items.Update(item);
+                _db.SaveChanges();
+                
+            }
+
+            ViewBag.CatId = new SelectList(_db.Categories, "CatId", "CatName");
+            return RedirectToAction("Index");
+        }
+
 
     }
 }
