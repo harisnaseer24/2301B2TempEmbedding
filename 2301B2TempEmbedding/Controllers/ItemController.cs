f5639d4b6a6029d96f62bef43602a5148983104f
harisnaseer24 <harisnaseer258@gmail.com>
2024-07-22 11:21:36 +0500

    CRUD OPERATTIONS COMPLETED with file uploading.

--- 2301B2TempEmbedding/Controllers/ItemController.cs
+++ 2301B2TempEmbedding/Controllers/ItemController.cs
@@ -75,7 +75,6 @@ namespace _2301B2TempEmbedding.Controllers
         public IActionResult Edit(Item item, IFormFile file, string oldimage)
         {
             var dbimage = "";
-
             if(file != null && file.Length > 0)
             {
                 var imageName = DateTime.Now.ToString("yymmddhhmmss");//24074455454454
@@ -83,20 +82,14 @@ namespace _2301B2TempEmbedding.Controllers
 
                 string imagepath = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/Uploads");
                 var imagevalue = Path.Combine(imagepath, imageName);
-
                 using (var stream = new FileStream(imagevalue, FileMode.Create))
                 {
-
                     file.CopyTo(stream);
-
                 }
-
                 dbimage = Path.Combine("/Uploads", imageName);//   /uploads/240715343434apple.png
                 item.Pimage = dbimage;
                 _db.Items.Update(item);
                 _db.SaveChanges();
-
-
 
             }
             else
@@ -106,11 +99,38 @@ namespace _2301B2TempEmbedding.Controllers
                 _db.SaveChanges();
                 
             }
-
             ViewBag.CatId = new SelectList(_db.Categories, "CatId", "CatName");
             return RedirectToAction("Index");
         }
 
+
+        public IActionResult Delete(int id)
+        {
+            ViewBag.CatId = new SelectList(_db.Categories, "CatId", "CatName");
+            var item1 = _db.Items.Find(id);
+            if(item1 == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+             return View(item1);
+            }
+           
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Item item)
+        {
+           
+                _db.Items.Remove(item);
+                _db.SaveChanges();
+
+            
+            ViewBag.CatId = new SelectList(_db.Categories, "CatId", "CatName");
+            return RedirectToAction("Index");
+        }
 
     }
 }
